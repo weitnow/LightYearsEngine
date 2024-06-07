2b450504b45b35cbb61617c111a93652be7fe28e
Christian Keusch <weitnow@gmail.com>
2024-06-07 13:57:53 +0200

    first window, its alive

--- LightYears/src/main.cpp
+++ LightYears/src/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
+    sf::RenderWindow renderWindow{sf::VideoMode(100, 100), "myWindow"};
+
+    while (renderWindow.isOpen())
+    {
+        sf::Event windowEvent; 
+        while (renderWindow.pollEvent(windowEvent))
+        {
+            if (windowEvent.type == sf::Event::EventType::Closed)
+            {
+                renderWindow.close();
+            }
+        }
+
+    }
     std::cout << "hello world!" << std::endl;
 }

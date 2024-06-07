ba043bc939aabbdb95410e04b5694cd5c9fd256c
Christian Keusch <weitnow@gmail.com>
2024-06-07 16:17:35 +0200

    renderloop implemented

--- LightYears/src/framework/Application.cpp
+++ LightYears/src/framework/Application.cpp
@@ -1,13 +1,21 @@
+#include <iostream>
+
 #include "framework/Application.h"
 
 namespace ly
 {
 	Application::Application()
-		: mWindow{ sf::VideoMode(1024, 1440), "Light Years"}
+		: mWindow{ sf::VideoMode(1024, 1440), "Light Years"},
+        mTargetFrameRate{60.f},
+        mTickClock{}
 	{
+
 	}
 	void Application::Run()
 	{
+        mTickClock.restart();
+        float accumulatedTime = 0.f;
+        float targetDeltaTime = 1.f / mTargetFrameRate; // 1 sec divided by 60
         while (mWindow.isOpen())
         {
             sf::Event windowEvent;
@@ -18,7 +26,24 @@ namespace ly
                     mWindow.close();
                 }
             }
-
+            float frameDeltaTime = mTickClock.restart().asSeconds(); //returns time since clock start in seconds and restarts it
+            accumulatedTime += frameDeltaTime;
+            while (accumulatedTime > targetDeltaTime)
+            {
+                accumulatedTime -= targetDeltaTime;
+                Tick(targetDeltaTime);
+                Render();
+            }
+            std::cout << "ticking at framerate: " << 1.f / frameDeltaTime << std::endl;
         }
 	}
+    void Application::Tick(float deltaTime)
+    {
+        
+    }
+
+    void Application::Render()
+    {
+
+    }
 }
